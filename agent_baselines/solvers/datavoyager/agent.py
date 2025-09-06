38501dd315d72cb6519dcd9f6255599803acf756
Mike D'Arcy <miked@allenai.org>
2025-09-05 17:00:13 -0700

    DV + task tools

--- agent_baselines/solvers/datavoyager/agent.py
+++ agent_baselines/solvers/datavoyager/agent.py
@@ -8,9 +8,15 @@ import os
 import tempfile
 from pathlib import Path
 
+from astabench.tools.stateful_python import get_sandbox_jupyter
 from astabench.util.model import record_model_usage_with_inspect
+from astabench.util.sandbox import SandboxToolManager
 from inspect_ai.model import ModelUsage
 from inspect_ai.solver import Generate, Solver, TaskState, solver
+from inspect_ai.tool import ToolDef
+from inspect_ai.util import sandbox
+
+from agent_baselines.solvers.smolagents.agent import make_tools_prompt
 
 logger = logging.getLogger(__name__)
 
@@ -22,9 +28,28 @@ DEFAULT_CONFIG = (
 )
 
 
+def _augmented_prompt(state: TaskState) -> str:
+    prompt = state.input_text
+
+    tools_usage_prompt = ""
+
+    tool_def_list = [ToolDef(t) for t in state.tools]
+    if tool_def_list:
+        tool_def_prompt = make_tools_prompt(tool_def_list)
+
+        tools_usage_prompt = f"""You have access to astabench tools in a sandbox environment. You can use these tools in your Python code:
+{tool_def_prompt}\n\n"""
+
+    general_instruction_prompt = tools_usage_prompt
+
+    # Augment the user prompt with tool instructions
+    return general_instruction_prompt + "\n\nTask:\n" + prompt
+
+
 @solver
 def datavoyager_solver(
     config_file: str = DEFAULT_CONFIG.as_posix(),
+    include_task_tools: bool = False,
 ) -> Solver:
     """This solver wraps the DataVoyager-core agent via bridge()."""
 
@@ -54,8 +79,31 @@ def datavoyager_solver(
                 enable_console_logging=True,
             )
 
-            response: str = await agent.run(query=state.input_text)
+            prompt = state.input_text
+
+            if include_task_tools:
+                tools = [t for t in state.tools if ToolDef(t).name != "python_session"]
+
+                inspect_tool_manager = SandboxToolManager(
+                    sandbox(),
+                    # Remove the python tool since we use exec_python_session directly
+                    tools=tools,
+                    # DV has its own final answer mechanism
+                    include_final_answer_tool=False,
+                )
+
+                jupy = await get_sandbox_jupyter()
+
+                await jupy.__aenter__()
+                await inspect_tool_manager.setup_tool_environment()
+                await jupy.run_code(inspect_tool_manager.get_sandbox_preamble())
+
+                prompt = _augmented_prompt(state)
+
+            response: str = await agent.run(query=prompt)
             state.output.completion = response
+            if include_task_tools:
+                await jupy.__aexit__(None, None, None)
 
             # Record model usage
             total_usages = agent.total_usage()
